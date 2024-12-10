9efe3bed262919137229b4e41a5aa02d0d9c894f
Waaatanuki <watanukipublic@gmail.com>
2024-12-10 17:11:37 +0800

    feat: 完成raft food食谱

--- src/pages/raft/dishes/index.vue
+++ src/pages/raft/dishes/index.vue
@@ -1,47 +1,138 @@
 <script setup lang="ts">
+import BBQ from './asset/BBQ.webp'
 import Catfish_Deluxe from './asset/Catfish_Deluxe.webp'
-import Cave_Mushroom from './asset/Cave_Mushroom.webp'
-import Chili from './asset/Chili.webp'
-import Raw_Beet from './asset/Raw_Beet.webp'
-import Raw_Catfish from './asset/Raw_Catfish.webp'
-import Raw_Potato from './asset/Raw_Potato.webp'
+import Coconut_Chicken from './asset/Coconut_Chicken.webp'
+import Head_Broth from './asset/Head_Broth.webp'
+import Hearty_Stew from './asset/Hearty_Stew.webp'
+import Mushroom_Omelette from './asset/Mushroom_Omelette.webp'
+import Salmon_Salad from './asset/Salmon_Salad.webp'
+import Shark_Dinner from './asset/Shark_Dinner.webp'
+import Simple_Fish_Stew from './asset/Simple_Fish_Stew.webp'
+import Steak_with_Jam from './asset/Steak_with_Jam.webp'
+import Sushi from './asset/Sushi.webp'
+import Vegetable_Soup from './asset/Vegetable_Soup.webp'
 
 const food = [{
-  name: 'Catfish_Deluxe',
+  name: '椰子鸡',
+  img: Coconut_Chicken,
+  hunger: 55,
+  thirst: 10,
+  bonus: 55,
+  recipe: [['蘑菇'], ['椰子'], ['生鸟腿'], ['生鸟腿']],
+}, {
+  name: '简易炖鱼',
+  img: Simple_Fish_Stew,
+  hunger: 40,
+  thirst: 0,
+  bonus: 45,
+  recipe: [['生鲱鱼', '生鲳鱼'], ['生鲱鱼', '生鲳鱼'], ['生土豆', '生甜菜'], ['生土豆', '生甜菜']],
+}, {
+  name: '头汤',
+  img: Head_Broth,
+  hunger: 20,
+  thirst: 30,
+  bonus: 100,
+  recipe: [['生土豆', '生甜菜'], ['牛奶'], ['毒河豚头'], ['毒河豚头']],
+}, {
+  name: '蔬菜汤',
+  img: Vegetable_Soup,
+  hunger: 40,
+  thirst: 0,
+  bonus: 35,
+  recipe: [['生土豆', '生甜菜'], ['生土豆', '生甜菜'], ['生土豆', '生甜菜'], ['生土豆', '生甜菜']],
+}, {
+  name: '蘑菇煎蛋卷',
+  img: Mushroom_Omelette,
+  hunger: 35,
+  thirst: 0,
+  bonus: 35,
+  recipe: [['生土豆'], ['蛋'], ['蛋'], ['蘑菇']],
+}, {
+  name: '鲨鱼晚宴',
+  img: Shark_Dinner,
+  hunger: 45,
+  thirst: 0,
+  bonus: 70,
+  recipe: [['生鲨鱼肉'], ['生鲨鱼肉'], ['蘑菇'], ['银海藻']],
+}, {
+  name: '果酱牛排',
+  img: Steak_with_Jam,
+  hunger: 40,
+  thirst: 5,
+  bonus: 40,
+  recipe: [['生土豆', '生甜菜'], ['生肉'], ['红浆果'], ['红浆果']],
+}, {
+  name: '烧烤',
+  img: BBQ,
+  hunger: 65,
+  thirst: 0,
+  bonus: 75,
+  recipe: [['生肉'], ['生鲭鱼'], ['生鸟腿'], ['蘑菇']],
+}, {
+  name: '寿司',
+  img: Sushi,
+  hunger: 50,
+  thirst: 0,
+  bonus: 65,
+  recipe: [['生鲭鱼', '生罗非鱼'], ['生鲭鱼', '生罗非鱼'], ['蛋'], ['银海藻']],
+}, {
+  name: '奢华鲶鱼',
   img: Catfish_Deluxe,
-  recipe: [[Raw_Potato, Raw_Beet], [Raw_Catfish], [Cave_Mushroom], [Chili]],
+  hunger: 40,
+  thirst: 0,
+  bonus: 55,
+  recipe: [['生土豆', '生甜菜'], ['生鯰魚'], ['蘑菇'], ['辣椒']],
+  extra: '跑得更快',
+}, {
+  name: '鲑鱼沙拉',
+  img: Salmon_Salad,
+  hunger: 40,
+  thirst: 10,
+  bonus: 55,
+  recipe: [['菠萝', '芒果'], ['生鲑鱼'], ['银海藻'], ['姜黄']],
+  extra: '提升肺活量',
+}, {
+  name: '丰盛炖菜',
+  img: Hearty_Stew,
+  hunger: 20,
+  thirst: 0,
+  bonus: 30,
+  recipe: [['生土豆'], ['蘑菇'], ['生肉'], ['杜松莓']],
+  extra: '免死一次',
 }]
 </script>
 
 <template>
-  <div>
-    <img src="./asset/Catfish_Deluxe.webp" alt="" srcset="">
-    <div h-20 w-20 flex flex-wrap>
-      <div h-10 w-10>
-        <img h-full w-full src="./asset/Raw_Potato.webp">
+  <div fc flex-wrap gap-2>
+    <div v-for="dish in food" :key="dish.name" w-60 border-1>
+      <div flex items-center justify-between border-b-1 p-2>
+        <div w-80px fc flex-col>
+          <img h-10 w-10 :src="dish.img">
+          <div>{{ dish.name }}</div>
+        </div>
+        <div flex flex-col items-start>
+          <div>
+            饥饿：{{ dish.hunger }}
+          </div>
+          <div>
+            口渴：{{ dish.thirst }}
+          </div>
+          <div>
+            奖励：{{ dish.bonus }}
+          </div>
+        </div>
       </div>
-      <div h-10 w-10>
-        <img h-full w-full src="./asset/Raw_Catfish.webp">
-      </div>
-      <div h-10 w-10>
-        <img h-full w-full src="./asset/Cave_Mushroom.webp">
-      </div>
-      <div h-10 w-10>
-        <img h-full w-full src="./asset/Chili.webp">
-      </div>
-    </div>
-  </div>
-  <div v-for="dish in food" :key="dish.name">
-    <img :src="dish.img">
-
-    <div h-20 w-20 flex flex-wrap>
-      <div v-for="item, index in dish.recipe" :key="index" h-10 w-10>
-        <img v-for="img in item" :key="img" h-full w-full :src="img">
+      <div relative p-2 text-start>
+        <li v-for="list, index in dish.recipe" :key="index">
+          {{ list.join(' or ') }}
+        </li>
+        <div v-if="dish.extra" absolute bottom-2 right-2 px-1 ring-2 ring-rose>
+          <div text-end>
+            十分钟内
+          </div>
+          <div>{{ dish.extra }}</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
