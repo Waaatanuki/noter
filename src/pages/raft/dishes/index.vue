ff94a70df5bae3afb4f4124ccf8e97e4f676a554
Waaatanuki <watanukipublic@gmail.com>
2024-12-10 18:12:39 +0800

    feat: 完成raft drink食谱

--- src/pages/raft/dishes/index.vue
+++ src/pages/raft/dishes/index.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import BBQ from './asset/BBQ.webp'
 import Catfish_Deluxe from './asset/Catfish_Deluxe.webp'
+import Coconut_Beat from './asset/Coconut_Beat.webp'
 import Coconut_Chicken from './asset/Coconut_Chicken.webp'
 import Head_Broth from './asset/Head_Broth.webp'
 import Hearty_Stew from './asset/Hearty_Stew.webp'
+import Mangonana from './asset/Mangonana.webp'
 import Mushroom_Omelette from './asset/Mushroom_Omelette.webp'
+import Red_Melon from './asset/Red_Melon.webp'
+import Redbeet_Shot from './asset/Redbeet_Shot.webp'
 import Salmon_Salad from './asset/Salmon_Salad.webp'
 import Shark_Dinner from './asset/Shark_Dinner.webp'
+import Silver_Smoothie from './asset/Silver_Smoothie.webp'
 import Simple_Fish_Stew from './asset/Simple_Fish_Stew.webp'
+import Simple_Smoothie from './asset/Simple_Smoothie.webp'
+import Spicy_Pineberry from './asset/Spicy_Pineberry.webp'
 import Steak_with_Jam from './asset/Steak_with_Jam.webp'
+import Strawberry_Colada from './asset/Strawberry_Colada.webp'
 import Sushi from './asset/Sushi.webp'
 import Vegetable_Soup from './asset/Vegetable_Soup.webp'
 
@@ -100,11 +108,103 @@ const food = [{
   recipe: [['生土豆'], ['蘑菇'], ['生肉'], ['杜松莓']],
   extra: '免死一次',
 }]
+
+const drink = [{
+  name: '香椰节拍',
+  img: Coconut_Beat,
+  hunger: 10,
+  thirst: 50,
+  bonus: 30,
+  recipe: [['椰子'], ['椰子'], ['椰子'], ['生甜菜根']],
+}, {
+  name: '简易奶昔',
+  img: Simple_Smoothie,
+  hunger: 15,
+  thirst: 40,
+  bonus: 55,
+  recipe: [['芒果'], ['菠萝'], ['椰子'], ['椰子']],
+}, {
+  name: '草莓可乐达',
+  img: Strawberry_Colada,
+  hunger: 10,
+  thirst: 30,
+  bonus: 50,
+  recipe: [['菠萝'], ['草莓'], ['椰子'], ['椰子']],
+}, {
+  name: '银色奶昔',
+  img: Silver_Smoothie,
+  hunger: 10,
+  thirst: 40,
+  bonus: 65,
+  recipe: [['香蕉'], ['芒果'], ['牛奶'], ['银海藻']],
+}, {
+  name: '芒果香蕉',
+  img: Mangonana,
+  hunger: 5,
+  thirst: 45,
+  bonus: 70,
+  recipe: [['芒果'], ['香蕉'], ['香蕉'], ['牛奶']],
+}, {
+  name: '红瓜',
+  img: Red_Melon,
+  hunger: 10,
+  thirst: 70,
+  bonus: 45,
+  recipe: [['红浆果'], ['草莓'], ['西瓜'], ['椰子']],
+}, {
+  name: '辣菠萝莓',
+  img: Spicy_Pineberry,
+  hunger: 0,
+  thirst: 10,
+  bonus: 25,
+  recipe: [['菠萝'], ['草莓'], ['草莓'], ['辣椒']],
+  extra: '游得更快',
+}, {
+  name: '甜菜根汁',
+  img: Redbeet_Shot,
+  hunger: 5,
+  thirst: 10,
+  bonus: 25,
+  recipe: [['生甜菜根'], ['生甜菜根'], ['椰子'], ['姜黄']],
+  extra: '提升生命上限',
+}]
 </script>
 
 <template>
   <div fc flex-wrap gap-2>
     <div v-for="dish in food" :key="dish.name" w-60 border-1>
+      <div flex items-center justify-between border-b-1 p-2>
+        <div w-80px fc flex-col>
+          <img h-10 w-10 :src="dish.img">
+          <div>{{ dish.name }}</div>
+        </div>
+        <div flex flex-col items-start>
+          <div>
+            饥饿：{{ dish.hunger }}
+          </div>
+          <div>
+            口渴：{{ dish.thirst }}
+          </div>
+          <div>
+            奖励：{{ dish.bonus }}
+          </div>
+        </div>
+      </div>
+      <div relative p-2 text-start>
+        <li v-for="list, index in dish.recipe" :key="index">
+          {{ list.join(' or ') }}
+        </li>
+        <div v-if="dish.extra" absolute bottom-2 right-2 px-1 ring-2 ring-rose>
+          <div text-end>
+            十分钟内
+          </div>
+          <div>{{ dish.extra }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div mt-4 fc flex-wrap gap-2>
+    <div v-for="dish in drink" :key="dish.name" w-60 border-1>
       <div flex items-center justify-between border-b-1 p-2>
         <div w-80px fc flex-col>
           <img h-10 w-10 :src="dish.img">
